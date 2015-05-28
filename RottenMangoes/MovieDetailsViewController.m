6f1e47a4c2449f1420331088391d19731245a708
Jacy Wang <jacywj@gmail.com>
2015-05-27 17:43:05 -0700

    modify review labels constrains

--- RottenMangoes/MovieDetailsViewController.m
+++ RottenMangoes/MovieDetailsViewController.m
@@ -20,6 +20,8 @@
     self.title = self.movie.title;
     self.titleAndYearLabel.text = [[[self.movie.title stringByAppendingString:@" ("] stringByAppendingString:[NSString stringWithFormat:@"%d", self.movie.year]] stringByAppendingString:@")"];
     self.mmpaRatingLabel.text = self.movie.mpaaRating;
+    self.mmpaRatingLabel.clipsToBounds = YES;
+    self.mmpaRatingLabel.layer.cornerRadius = 5.0f;
     self.runtimeLabel.text = [NSString stringWithFormat:@"%d mins", self.movie.runtime];
     self.releaseDateLabel.text = self.movie.releaseDateInTheatre;
     self.synopsisLabel.text = self.movie.synopsis;
